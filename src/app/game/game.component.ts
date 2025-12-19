7b7325bd62e37e09e8e7b4e5fb5766c86cb8ecc7
Andreas Kissner <andikiss500@gmail.com>
2025-12-19 14:28:55 +0100

    Add cards shuffle

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
 export class GameComponent {
+  pickCardAnimation = false;
+  game! : Game;
+  currentCard : string | undefined = "";
 
+  constructor(){
+    this.newGame();
+  }
+
+  newGame(){
+   this.game = new Game();
+   console.log(this.game)
+  }
+  
+  takeCard() {
+    if(!this.pickCardAnimation){
+    this.currentCard = this.game.stack.pop();
+    console.log(this.currentCard);
+    this.pickCardAnimation = true;
+
+    setTimeout(()=>{
+      this.pickCardAnimation = false;
+    },1500)
+  }
+  }
 }
