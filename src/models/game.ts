91f4807cd4c62c015d2bc141db595accaa26d3df
Andreas Kissner <andikiss500@gmail.com>
2025-12-19 16:46:21 +0100

    Add Angular MAterials

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,7 +1,7 @@
 export class Game {
-  public players: string[] = [];
+  public players: string[] =[ "Andreas", "Leila", "Rana", "Salsa"];
   public stack: string[] = [];
-  public playedCard: string[] = [];
+  public playedCards: string[] = [];
   public currentPlayer: number = 0;
 
   constructor() {
